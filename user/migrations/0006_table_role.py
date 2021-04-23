8f67157a3803967dceb9aa024f7131eb424055e0
Jeffrey Prado <Jefgapra@espol.edu.ec>
2021-04-23 17:30:26 -0500

    app(user): modelo rol finalizado y modelo miembro actualizado

--- user/migrations/0006_table_role.py
+++ user/migrations/0006_table_role.py
@@ -17,18 +17,18 @@ class Migration(migrations.Migration):
             name='role',
         ),
         migrations.CreateModel(
-            name='Role',
+            name='MemberRole',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(choices=[('A', 'Presidente'), ('B', 'Vicepresidente'), ('C', 'Secretario'), ('D', 'Tesorero'), ('E', 'Vocal'), ('F', 'Miembro'), ('N', 'Ninguno'), ('X', 'Asesor'), ('Y', 'Tutor'), ('Z', 'Externo')], default='F', max_length=2)),
                 ('date_start', models.DateTimeField()),
                 ('date_end', models.DateTimeField()),
-                ('id_miembro', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('id_member', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
             model_name='member',
             name='actual_role',
-            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, to='user.role'),
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, to='user.memberrole'),
         ),
     ]
--- user/models.py
+++ user/models.py
@@ -47,14 +47,14 @@ class Person(models.Model):
     signature = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.card_id} {self.get_full_name}"
+        return f"{self.card_id}"
 
 class Professor(models.Model):
     id_person = models.OneToOneField('Person', on_delete=models.CASCADE)
     id_faculty = models.OneToOneField('institution.Faculty', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f"{self.id_person.get_full_name}"
+        return f"{self.id_person}"
 
 class Student(models.Model):
     enrollment_id = models.CharField(max_length=9, primary_key=True)
@@ -65,16 +65,16 @@ class Student(models.Model):
     id_person = models.OneToOneField('Person', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.matricula} {self.id_person.get_full_name}"
+        return f"{self.enrollment_id}"
     
 class Member(AbstractUser):
     id_student = models.OneToOneField('Student', on_delete=models.CASCADE, blank=True, null=True)
     description = models.CharField(max_length=100)
     date_joined = models.DateField(auto_now=True)
-    actual_role = models.OneToOneField('Role', on_delete=models.CASCADE, null=True)
+    actual_role = models.OneToOneField('MemberRole', on_delete=models.CASCADE, null=True)
     permissions = models.CharField(max_length=2, choices=PERMISSIONS, default='N')
-    ambassador = models.URLField(max_length=300, null=True, unique=True, validators=[ambassador_valid_url])
-    social_links = models.JSONField(null=True)
+    ambassador = models.URLField(max_length=300, blank=True, null=True, unique=True, validators=[ambassador_valid_url])
+    social_links = models.JSONField(blank=True, null=True)
     active = models.BooleanField(default=True)
     id_sub_org = models.ForeignKey('institution.SubOrganization', on_delete=models.CASCADE, blank=True, null=True)
 
@@ -82,16 +82,29 @@ class Member(AbstractUser):
         verbose_name_plural = "Members"
 
     def __str__(self):
-        if self.id_student:
-            return f"{self.id_student.id_person.get_full_name}"
+        # if self.id_student:
+        #     return f"{self.get_full_name}"
         return f"{self.username}"
 
-class Role(models.Model):
+class MemberRole(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=2, choices=ROLES, default='F')
-    id_miembro = models.ForeignKey('Member', on_delete=models.CASCADE, blank=True, null=True)
+    id_member = models.ForeignKey('Member', on_delete=models.CASCADE, blank=True, null=True)
     date_start = models.DateTimeField(auto_now=False)
     date_end = models.DateTimeField(auto_now=False)
 
     def __str__(self):
-        return f"{self.id_miembro} {self.name}"
-
+        return f"{self.id_member} {self.name}"
+    
+    def save(self, *args, **kwargs):
+        try:
+            member = Member.objects.get(id=self.id_member.id)
+            if member:
+                member.actual_role = self
+                member.save()
+            else: 
+                raise Exception("Error al tratar de asignar el rol actual.")
+            super().save(*args, **kwargs)
+        except Exception as e:
+            return f"{e}"
+            

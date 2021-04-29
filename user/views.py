908e74f5d338a20a65759d8f42cb16d0752a9ab1
cesar <cessvera@espol.edu.ec>
2021-04-29 00:57:06 -0500

    Creacion de viewset en user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework import permissions
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 # Create your views here.
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class MemberRoleViewSet(viewsets.ModelViewSet):
+    queryset = MemberRole.objects.all()
+    serializer_class = MemberRoleSerializer
+    permission_classes = [permissions.IsAuthenticated]

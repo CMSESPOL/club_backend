2340486239006fabf82a21bb2556bcf76e60e629
cesar <cessvera@espol.edu.ec>
2021-04-29 01:24:55 -0500

    Importacion de los viewset de user

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from institution.views import *
+from user.views import *
 
 router = routers.DefaultRouter()
 router.register(r'facultades', FacultyViewSet)
 router.register(r'carreras', CareerViewSet)
 router.register(r'organizacion', OrganizationViewSet)
 router.register(r'suborganizacion', SubOrganizationViewSet)
+router.register(r'persona', PersonViewSet)
+router.register(r'profesor', ProfessorViewSet)
+router.register(r'estudiante', StudentViewSet)
+router.register(r'miembro', MemberViewSet)
+router.register(r'rol de miembro', MemberRoleViewSet)
 
 
 urlpatterns = [

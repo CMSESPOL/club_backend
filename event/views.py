9bf1175636e6a1006f22714ed71f084875bca94b
RogwiCajas <rogwinalex2@hotmail.com>
2021-05-09 18:09:50 -0500

    app(event): creacion put&delete para evento

--- event/views.py
+++ event/views.py
@@ -36,7 +36,20 @@ class EventDetail(APIView):
         event = self.get_object(pk)
         serializer = EventSerializer(event)
         return Response(serializer.data)
-        
+    
+    def put(self, request, pk, format=None):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class DocumentList(APIView):
     permission_classes = [permissions.IsAuthenticated]
 

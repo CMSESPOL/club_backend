a15ace14b248175b364c2f256b4e37fab346fd51
RogwiCajas <rogwinalex2@hotmail.com>
2021-05-13 10:23:43 -0500

    app(event):endpoint ordenado y por  organizacion-evento

--- event/views.py
+++ event/views.py
@@ -23,39 +23,6 @@ class EventList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class EventOrderedList(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_order_objects(self, order):
-        try:
-            if(order=='dsc'):
-                return Event.objects.order_by('-date_start')
-            else:
-                return Event.objects.order_by('date_start')
- 
-        except Event.DoesNotExist:
-            raise Http404
-
-    def get(self, request, order, format=None):
-        events = self.get_order_objects(order)
-        serializer = EventSerializer(events, many=True)
-        return Response(serializer.data)
-
-class EventByTypeList(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_Bytype_objects(self, event_type):
-        try:
-            return Event.objects.filter(event_type=event_type)
- 
-        except Event.DoesNotExist:
-            raise Http404
-
-    def get(self, request, event_type, format=None):
-        events = self.get_Bytype_objects(event_type)
-        serializer = EventSerializer(events, many=True)
-        return Response(serializer.data)
-
 class EventDetail(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -65,11 +32,36 @@ class EventDetail(APIView):
         except Event.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
-        event = self.get_object(pk)
-        serializer = EventSerializer(event)
+    def get_by_type_objects(self, id_organization, event_type):
+        try:
+            return Event.objects.filter(id_suborg_in_charge=id_organization).filter(event_type=event_type)
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get_by_order_objects(self, order):
+        try:
+            if(order=='dsc'):
+                return Event.objects.order_by('-date_start')
+            else:
+                return Event.objects.order_by('date_start')
+        except Event.DoesNotExist:
+            raise Http404
+ 
+    def get(self, request, pk=None, order=None, id_organization=None, event_type=none, format=None):
+       
+        if pk:
+            event = self.get_object(pk)
+            serializer = EventSerializer(event)
+        
+        if order:
+            events = self.get_by_order_objects(order)
+            serializer = EventSerializer(events, many=True)
+        if id_organization and event_type:
+            events = self.get_by_type_objects(id_organization, event_type)
+            serializer = EventSerializer(events, many=True)
+        
         return Response(serializer.data)
-    
+        
     def put(self, request, pk, format=None):
         event = self.get_object(pk)
         serializer = EventSerializer(event, data=request.data)

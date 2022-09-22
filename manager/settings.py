f007d2463ed6cb81be60b131626d02135fdc94e4
Administrator@CELULA.ESPOL.EDU.EC <Administrator@CELULA.ESPOL.EDU.EC>
2022-09-22 06:56:51 -0500

    proyecto(manager): static files release

--- manager/settings.py
+++ manager/settings.py
@@ -120,6 +120,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
